549ae2ad5c4c3b0230d985b9f8027c0bfa33e04c
ganapatiKabbargi <ganapatisk96@gmail.com>
2022-11-03 10:49:27 +0530

    storage feature added

--- file.js
+++ file.js
@@ -1,28 +1,10 @@
-const formEl=document.querySelector(".form")
+        const formEl=document.querySelector(".form")
         const ul=document.querySelector(".items")
         const btn=document.querySelector(".btn")
         const nameEl=document.querySelector("#username")
         const emailEl=document.querySelector("#email")
 
-        formEl.addEventListener("click",(e)=>{
-            formEl.style.backgroundColor="lightgrey"
-        })
-        nameEl.addEventListener("mouseover",(e)=>{
-            e.preventDefault()
-            nameEl.style.transform="scale(1.1)"
-        })
-        emailEl.addEventListener("mouseover",(e)=>{
-            e.preventDefault()
-            emailEl.style.transform="scale(1.1)"
-        })
-        nameEl.addEventListener("mouseout",(e)=>{
-            e.preventDefault()
-            nameEl.style.transform="scale(1)"
-        })
-        emailEl.addEventListener("mouseout",(e)=>{
-            e.preventDefault()
-            emailEl.style.transform="scale(1)"
-        })
+       
         formEl.addEventListener("submit", onSubmit)
 
         function onSubmit(e){
@@ -30,12 +12,9 @@ const formEl=document.querySelector(".form")
            if(nameEl.value === '' || emailEl.value === '' ){
             alert("Please fill all the fields")
            }else{
-            btn.style.backgroundColor="green"
-            btn.value="Submitted"
-            console.log(nameEl.value)
-            console.log(emailEl.value) 
+            localStorage.setItem("username",nameEl.value)
+            localStorage.setItem("email",emailEl.value)
            }
             nameEl.value="";
-            emailEl.value="";
-           
+            emailEl.value="";  
         }
